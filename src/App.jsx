3512c303c77be10f87938a1a7ca8211b4837c3d0
PerikK <perik76@gmail.com>
2024-09-07 19:43:56 +0300

    create and render components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from 'react'
-import './App.css'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+import Home from './components/Home.jsx'
+import Navbar from './components/Navbar.jsx'
+import Skills from './components/Skills.jsx'
+import Projects from './components/Projects.jsx'
+
 
 function App() {
 
   return (
-    <></>
+      <div className='bg-yellow-600 text-black text-5xl font-extrabold p-5 border-8 border-black border-dashed rounded-3xl ' >
+              <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
+
+
   )
 }
 
