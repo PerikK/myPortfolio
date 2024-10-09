20ede033c228218b76c551a1b6550ad0bdd7953f
PerikK <perik76@gmail.com>
2024-10-09 21:50:59 +0300

    modal

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -1,16 +1,43 @@
-export default function ProjectModal({ isOpen, onClose, children }) {
+export default function ProjectModal({ isOpen, onClose, images }) {
+	const handleEscape = (event) => {
+		if (event.key === 'Escape') {
+			onClose()
+		}
+	}
+
+	if (isOpen) {
+		document.addEventListener('keydown', handleEscape)
+	}
+
+	const handleBackdropClick = (event) => {
+		if (event.target === event.currentTarget) {
+			onClose()
+		}
+	}
 	if (!isOpen) return null
 
 	return (
-		<div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75'>
-			<div className='relative p-4 bg-white rounded-md shadow-lg max-w-full max-h-full overflow-auto'>
+		<div
+			className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'
+			onClick={handleBackdropClick}
+		>
+			<div className='bg-gray-300 bg-opacity-30 rounded-lg shadow-lg p-4 max-w-7xl'>
 				<button
-					className='absolute top-0 right-0 m-2 text-2xl text-black bg-white rounded-full p-1'
 					onClick={onClose}
+					className='absolute top-2 right-2 text-gray-500'
 				>
-					X
+					✖
 				</button>
-				{children}
+				<div className='grid grid-cols-2 gap-4'>
+					{images.map((src, index) => (
+						<img
+							key={index}
+							src={src}
+							alt={`Project Image ${index}`}
+							className='rounded-lg'
+						/>
+					))}
+				</div>
 			</div>
 		</div>
 	)
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react'
 import code from '../assets/code2.jpg'
 import ProjectModal from './ProjectModal'
+import be1 from '../assets/project_images/cm_be/be1.png'
+import be2 from '../assets/project_images/cm_be/be2.png'
+import fe1 from '../assets/project_images/cm_fe/fe1.png'
+import fe2 from '../assets/project_images/cm_fe/fe2.png'
+import fe3 from '../assets/project_images/cm_fe/fe3.png'
+import fe4 from '../assets/project_images/cm_fe/fe4.png'
 
 export default function Projects() {
-	const [isOpen, setIsOpen] = useState(false)
+	const [isModalOpen, setIsModalOpen] = useState(false)
+	const [selectedImages, setSelectedImages] = useState([])
+
+	const beImages = [be1, be2]
+	const feImages = [fe1, fe3, fe2, fe4]
+
+	const openModal = (images) => {
+		setSelectedImages(images)
+		setIsModalOpen(true)
+	}
 
 	return (
 		<div
@@ -18,9 +34,7 @@ export default function Projects() {
 						Check out some of my projects
 					</p>
 				</div>
-				{/* Container */}
 				<div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-					{/* Grid Item */}
 					<div
 						style={{ backgroundImage: `url(${code})` }}
 						className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
@@ -28,7 +42,7 @@ export default function Projects() {
 						{/* Hover Effects */}
 						<div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
 							<span className=' text-lg font-bold text-white tracking-wider'>
-								Cohort Manager Fron-End
+								Cohort Manager Front-End
 							</span>
 							<p className='text-center text-sm pt-2'>
 								The front-end of a school management app, that allows
@@ -36,11 +50,12 @@ export default function Projects() {
 								arrange classes. Built with React in an Agile setting
 							</p>
 							<div className='pt-8 text-center'>
-								<a href='/'>
-									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-										Demo
-									</button>
-								</a>
+								<button
+									className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'
+									onClick={() => openModal(feImages)}
+								>
+									Demo
+								</button>
 								<a
 									href='https://github.com/boolean-uk/team-dev-client-c12'
 									target='_blank'
@@ -68,11 +83,12 @@ export default function Projects() {
 								setting
 							</p>
 							<div className='pt-8 text-center'>
-								<a href=''>
-									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-										<ProjectModal />
-									</button>
-								</a>
+								<button
+									className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'
+									onClick={() => openModal(beImages)}
+								>
+									Demo
+								</button>
 								<a
 									href='https://github.com/boolean-uk/team-dev-server-c12'
 									target='_blank'
@@ -191,6 +207,11 @@ export default function Projects() {
 							</div>
 						</div>
 					</div>
+					<ProjectModal
+						isOpen={isModalOpen}
+						onClose={() => setIsModalOpen(false)}
+						images={selectedImages}
+					/>
 				</div>
 			</div>
 		</div>
