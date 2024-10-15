52ab9c0aec18b50b1a7b253b857f8327d197337a
PerikK <perik76@gmail.com>
2024-10-15 22:11:44 +0300

    refactor for mobile responsivenes -2nd part

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,39 +1,39 @@
 export default function About() {
     return (
-        <div
-      name="about"
-      id="about"
-      className="w-full h-screen bg-[#0a192f] text-gray-300"
-    >
-      <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className=" py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-4/6">
-          <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8 mb-4">
-            <div className="sm:text-right pb-8 pl-4">
-              <p className="text-4xl font-bold inline border-b-4 border-cyan-500">
-                About
-              </p>
+<div
+            name="about"
+            id="about"
+            className="w-full min-h-screen bg-[#0a192f] text-gray-300 py-16"
+        >
+            <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+                <div className="pb-8 text-center">
+                    <p className="text-4xl font-bold inline border-b-4 border-cyan-500">
+                        About
+                    </p>
+                </div>
+                
+                <div className="bg-cyan-800 rounded-md p-8 mt-4">
+                    <div className="grid sm:grid-cols-2 gap-8">
+                        <div className="text-2xl sm:text-3xl font-bold">
+                            <p>
+                                Hi. I'm Periklis Kafchitsas. Nice to meet you. Feel free to take a 
+                                look around and see if we could work together.
+                            </p>
+                        </div>
+                        <div className="text-base sm:text-lg">
+                            <p>                
+                                I'm a Full Stack Developer who recently completed an intensive 6-month, 
+                                <span className='font-sans'> ( {'>'} 900 hours ) course at </span>
+                                <a href="https://boolean.co.uk" className="text-cyan-300 hover:text-cyan-100 transition-colors">Boolean UK</a>.
+                                I gained hands-on experience in front-end and back-end technologies, building full-stack projects, 
+                                working with RESTful APIs, React, PostgreSQL, managing team simulations, documentation, and agile methodologies. 
+                                I love creating useful applications and especially enjoy creating Back-ends and APIs.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div></div>
-          </div>
-          <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-            <div className="sm:text-right text-4xl font-bold">
-              <p>
-                Hi. I'm Periklis Kafchitsas. Nice to meet you. Fell free to take a 
-                look around and see if we could work together.
-              </p>
-            </div>
-            <div>
-              <p>                
-                I'm Periklis. A Full Stack Developer who recently completed an intensive 6-month, <section className='font-sans'>
-                            ( {'>'} 900 hours )
-                        course at <a href="https://boolean.co.uk">Boolean UK</a>.
-                        </section> I gained hands-on experience in front-end and back-end technologies, building full-stack projects, working with RESTful APIs, React, PostgreSQL, managing team simulations, documentation, and agile methodologies. I love creating usefull applications and especially enjoy creating Back-ends and APIs.
-              </p>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
     )
 }
 
