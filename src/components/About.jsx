662454f6bcda83fd555f05af34bac38ed5417038
PerikK <perik76@gmail.com>
2024-10-16 11:24:33 +0300

    more refining in the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,18 +20,20 @@ export default function About() {
 						<div className='grid sm:grid-cols-2 gap-8'>
 							<div className='text-2xl sm:text-3xl font-bold'>
 								<p>
-									Hi, I’m Periklis Kafchitsas. Nice to have you visit my portfolio . Feel
-									free to take a look around and see if we could work
-									together.
+									Hi, I’m Periklis Kafchitsas. Nice to have you visit
+									my portfolio . Feel free to take a look around and
+									see if we could work together.
 								</p>
 							</div>
 							<div className='text-base sm:text-lg'>
-                <p>
-                  I 've been working for 20 years as an IT/Network administrator and support technician. 
-                  In this period, I have worked for various companies as well as a freelancer.
-                  Last year I decided to re-skill and become a Full Stack Developer. <br/>
-                  I recently completed an
-									intensive 6-month,
+								<p>
+									I 've been working for 20 years as an IT/Network
+									administrator and support technician. In this
+									period, I have worked for various companies as well
+									as a freelancer. <br /> Last year I decided to make
+									a turn in my career and become a Full Stack Software
+									Developer. <br />I recently graduated from an
+									intensive <br /> 6-month,
 									<span className='font-sans'>
 										{' '}
 										( {'>'} 900 hours ) course at{' '}
@@ -42,31 +44,33 @@ export default function About() {
 									>
 										Boolean UK
 									</a>
-									. I gained hands-on experience in front-end and
+									, where I gained hands-on experience in front and
 									back-end technologies, building full-stack projects,
-									working with RESTful APIs, React, PostgreSQL,
-									managing team simulations, documentation, and agile
-									methodologies. I love creating useful applications
-									and especially enjoy creating Back-ends and APIs.
+									working with RESTful APIs, React, ExpressJS, PostgreSQL,
+									managing team simulations, documentation, using
+									Agile methodologies. I love creating useful
+									applications and especially enjoy creating Back-ends
+									and APIs. I also love to work in teams and I feel
+									great to work in an environment with clearly
+									outlined goals that I do my best to achieve.
 								</p>
 							</div>
 						</div>
 					</div>
-				<div>
-					<Link
-						to='skills'
-						smooth
-						duration={500}
-						className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'
-					>
-						Skills
-						<span className='group-hover:rotate-90 duration-300'>
-							<HiArrowNarrowRight size={25} className='ml-3' />
-						</span>
-					</Link>
+					<div>
+						<Link
+							to='skills'
+							smooth
+							duration={500}
+							className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'
+						>
+							Skills
+							<span className='group-hover:rotate-90 duration-300'>
+								<HiArrowNarrowRight size={25} className='ml-3' />
+							</span>
+						</Link>
+					</div>
 				</div>
-				</div>
-
 			</div>
 		</>
 	)
