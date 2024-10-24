f0a0ac41d840c3c786c8b5db0d925856ba919fc0
Periklis <PerikK@users.noreply.github.com>
2024-10-25 02:20:45 +0300

    fix mobile responsiveness for project images modal

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -7,6 +7,7 @@ export default function ProjectModal({ isOpen, onClose, images }) {
 
 	if (isOpen) {
 		document.addEventListener('keydown', handleEscape)
+		console.log(images);
 	}
 
 	const handleBackdropClick = (event) => {
@@ -21,20 +22,20 @@ export default function ProjectModal({ isOpen, onClose, images }) {
 			className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'
 			onClick={handleBackdropClick}
 		>
-			<div className='bg-gray-300 bg-opacity-30 rounded-lg shadow-lg p-4 max-w-9xl h-full'>
+			<div className='bg-gray-300 bg-opacity-30 rounded-lg shadow-lg p-4 max-w-9xl h-4/5 m-3'>
 				<button
 					onClick={onClose}
 					className='absolute top-2 right-2 text-gray-500'
 				>
-					✖
+					Press Esc or click -&gt; ✖
 				</button>
-				<div className='grid grid-cols-2 gap-4'>
+				<div className='m-3 md:grid md:grid-cols-2 md:gap-4'>
 					{images.map((src, index) => (
 						<img
 							key={index}
 							src={src}
 							alt={`Project Image ${index}`}
-							className='rounded-lg mx-auto w-3/4 md:w-full h-3/4 md:h-full'
+							className='rounded-lg my-8 '
 						/>
 					))}
 				</div>
