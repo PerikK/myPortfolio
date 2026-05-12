cd6e02f84ee1ce51ad8dc6d65161ce40162aa807
periklis.kafchitsas <periklis.kafchitsas@enorainnovation.com>
2026-05-12 22:32:11 +0300

    fix env errors

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-dotenv.config()
+// dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	define: {
-		'process.env': process.env,
-	},
+	// define: {
+	// 	'process.env': process.env,
+	// },
 })
